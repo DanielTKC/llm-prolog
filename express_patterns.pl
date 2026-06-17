25b48c0949cce0ce1758e58a0ca0b65529bc4d37
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2026-06-17 09:34:08 -0500

    Dynamic routes and clearing

--- express_patterns.pl
+++ express_patterns.pl
@@ -1,1 +1,12 @@
-:- module(express_patterns, []).
+:- module(express_patterns, [
+    route/4,
+    assert_route/1,
+    clear_routes/0
+]).
+
+:- dynamic route/4.
+assert_route(route(Name, Method, Path, Features)) :-
+    assertz(route(Name, Method, Path, Features)).
+
+clear_routes :-
+    retractall(route(_,_,_,_)).
